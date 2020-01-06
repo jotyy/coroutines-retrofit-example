3ff0bd102d4f1c1fa6bdaa8e0eceacb90fb3d6bb
Jotyy <838373812@qq.com>
2020-01-06 08:39:18 +0800

    initial commit

---
+++ app/src/main/java/top/jotyy/coroutines_retrofit_example/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package top.jotyy.coroutines_retrofit_example.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
